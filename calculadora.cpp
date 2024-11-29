8a6ea0d4331bfd2365975376e8186493cfa576a5
root-Tex <175974114+root-Tex@users.noreply.github.com>
2024-11-28 21:45:25 -0400

    Funciones de sumar y restar

---
+++ calculadora.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, totalsuma, totalresta;
+
+    cout << "Ingrese el primer numero --> ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero --> ";
+    cin >> num2;
+
+    totalsuma = num1 + num2;
+    totalresta = num1 - num2;
+
+    cout << "RESULTADO ES: \n";
+    cout << "Suma --> " << totalsuma << "\n";
+    cout << "Resta --> " << totalresta << "\n";
+
+    return 0; 
+}
