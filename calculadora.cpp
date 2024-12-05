8a7d95ccc7e8da2a898356d7e42c0a52b60483f9
JelYsR00T <jelytosr00t@gmail.com>
2024-12-04 23:16:48 -0400

    Creación de funciones de raiz y potencia

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
     int num1, num2, totalsuma, totalresta;
+    double potencia, raiz_num1, raiz_num2;
 
     cout << "Ingrese el primer numero --> ";
     cin >> num1;
@@ -13,9 +15,18 @@ int main()
     totalsuma = num1 + num2;
     totalresta = num1 - num2;
 
+	// Operaciones adicionales: Potenciación y raíz cuadrada
+    potencia = pow(num1, num2); // num1 elevado a la potencia de num2
+    raiz_num1 = sqrt(num1);    // Raíz cuadrada de num1
+    raiz_num2 = sqrt(num2);    // Raíz cuadrada de num2
+
+	// Resultados
     cout << "RESULTADO ES: \n";
     cout << "Suma --> " << totalsuma << "\n";
     cout << "Resta --> " << totalresta << "\n";
+    cout << "Potenciacion (" << num1 << "^" << num2 << ") --> " << potencia << "\n";
+    cout << "Raiz cuadrada de " << num1 << " --> " << raiz_num1 << "\n";
+    cout << "Raiz cuadrada de " << num2 << " --> " << raiz_num2 << "\n";
 
     return 0; 
 	
